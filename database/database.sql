151192c837ccb6508343338a02d13bfaef8a6f6c
Locitao <rvdtera@gmail.com>
2021-04-11 12:30:26 +0200

    Update database with fixed score.
    Fix the score for Lord of the Rings, since they are 11/10 movies (not counting the Hobbit movies of course).

--- database/database.sql
+++ database/database.sql
@@ -10,7 +10,7 @@ CREATE TABLE movies (
 INSERT INTO movies VALUES ("The Dark Knight", 8.1);
 INSERT INTO movies VALUES ("Inception", 5.7);
 INSERT INTO movies VALUES ("The Matrix", 9.9);
-INSERT INTO movies VALUES ("The Lord of the Rings", 7.8);
+INSERT INTO movies VALUES ("The Lord of the Rings", 11.0);
 INSERT INTO movies VALUES ("Star Wars", 3.4);
 INSERT INTO movies VALUES ("Saving Private Ryan", 4.5);
 INSERT INTO movies VALUES ("The Green Mile", 10.0);
